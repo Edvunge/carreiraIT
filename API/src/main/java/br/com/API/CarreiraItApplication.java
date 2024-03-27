2aee526d3b9b31d5199fca669f46db794f9eb4d1
edvunge <costaedy18@gmail.com>
2024-03-27 09:49:26 +0000

    first commit

---
+++ API/src/main/java/br/com/API/CarreiraItApplication.java
@@ -0,0 +1,13 @@
+package br.com.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarreiraItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarreiraItApplication.class, args);
+	}
+
+}
